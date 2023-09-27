990d8edd668d7fbb13f4911cede191310cabf499
Vihenrie <viniciushpintolp@gmail.com>
2023-09-27 08:58:18 -0300

    tailwind

---
+++ src/pages/Mapa/index.tsx
@@ -0,0 +1,9 @@
+import imagem_1 from '../../assets/Paisagens/imagem_1_v2.jpg'
+
+export default function Viagem() {
+    return(
+        <div className='filter blur 21'>
+            <img src={imagem_1} alt="background" />
+        </div>
+    )
+}
